522e4b6857799456acf4c511790e1c186457a594
Inchara N <inchara882@gmail.com>
2026-08-18 15:32:39 +0530

    Added labels instead of p tag

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,40 +1,42 @@
 import { useState } from "react";
 
-export default function App (){
+export default function App() {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
   }
 
-  function handleReset(){
+  function handleReset() {
     setName("");
     setEmail("");
   }
-  return(
+  return (
     <div>
       <h2>Controlled component</h2>
       <form onSubmit={handleSubmit}>
-      <p>Name: </p>
-      <input 
-      type="text" 
-      value={name}
-      onChange={(e) => setName(e.target.value)}
-      /> <br />
+          <label>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          /> <br />
+    
 
-      <input 
-      type="email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      /> 
-      <br />
-      <button type="submit">Submit</button> <br />
-      <button type="submit" onClick={handleReset}>Reset</button>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <button type="submit">Submit</button> <br />
+        <button type="submit" onClick={handleReset}>Reset</button>
       </form>
-      <p>Name: {name}</p>
-      <p>Email: {email}</p>
-      
+      {/* <p>Name: {name}</p>
+      <p>Email: {email}</p> */}
+
     </div>
   )
 }
