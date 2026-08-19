19f26f40eb91d915aa4c2eb41ba195b34485cb3e
Inchara N <inchara882@gmail.com>
2026-08-19 15:24:49 +0530

    Added form validation

--- controlled-form-assignment/src/App.jsx
+++ controlled-form-assignment/src/App.jsx
@@ -5,24 +5,65 @@ export default function App() {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [gender, setGender] = useState("")
-  const [agree, setAgree] = useState("")
+  const [agree, setAgree] = useState(false)
   const [course, setCourse] = useState("")
+
+  const [errors, setErrors] = useState({});
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if(validate()){
+
     console.log("Name:", name);
     console.log("Email:", email);
     console.log("Gender:", gender);
     console.log("Agree:", agree);
     console.log("Course:", course);
+
+    
+      alert("Form submitted successfully!")
+    }
+  };
+  
+  
+  const validate = () => {
+    let newErrors = {};
+
+    if(name.trim() === ""){
+      newErrors.name = "Name is required";
+    }
+
+    if (email.trim() === "") {
+      newErrors.email = "Email is required";
+    } else if (!email.includes("@")) {
+      newErrors.email = "Enter a valid email";
+    }
+
+    if(gender.trim() ===""){
+      newErrors.gender = "Please select gender"
+    }
+
+    if(!agree){
+      newErrors.agree = "Agree to the terms and conditions";
+    }
+
+    if(course.trim() ===""){
+      newErrors.course = "Please select a course"
+    }
+
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
   }
 
   function handleReset() {
     setName("");
     setEmail("");
     setGender("");
-    setAgree("");
+    setAgree(false);
     setCourse("");
+    setErrors({});
   }
   return (
     <div className="container">
@@ -34,7 +75,10 @@ export default function App() {
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
-        /> <br />
+        /> 
+        {errors.name && <p>{errors.name}</p>}
+        <br />
+        
         </div>
 
         <div className="form-row">
@@ -43,7 +87,9 @@ export default function App() {
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-        /><br />
+        />
+        {errors.email && <p>{errors.email}</p>}
+        <br />
         </div>
 
         <div className="form-row">
@@ -55,7 +101,7 @@ export default function App() {
         value="Male"
         checked={gender==="Male"}
         onChange={(e) => setGender(e.target.value)}
-         />
+         /> {errors.gender && <p>{errors.gender}</p>}
         Male </label> 
 
         <label>
@@ -65,7 +111,7 @@ export default function App() {
         value="Female"
         checked={gender==="Female"}
         onChange={(e) => setGender(e.target.value)}
-         />
+         /> {errors.gender && <p>{errors.gender}</p>}
         Female </label> <br />
         </div>
 
@@ -78,6 +124,7 @@ export default function App() {
            />
            I agree to all terms and conditions.
         </p>
+        {errors.agree && <p>{errors.agree}</p>}
         </div>
 
         <div className="form-row">
@@ -91,11 +138,13 @@ export default function App() {
         <option value="React">React</option>
         <option value="SQL">SQL</option>
         <option value="Angular">Angular</option>
-        </select> <br /> <br />
+        </select> 
+        {errors.course && <p>{errors.course}</p>}
+        <br /> <br />
         </div>
 
         <button type="submit">Submit</button> 
-        <button type="submit" onClick={handleReset}>Reset</button>
+        <button type="button" onClick={handleReset}>Reset</button>
       </form>
     </div>
   )
