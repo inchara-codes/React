8afbe0be7c5dbc7ad5e5f8bddcc33a327433c750
Inchara N <inchara882@gmail.com>
2026-08-20 11:10:45 +0530

    Added email input and label's to them

--- uncontrolled-component/src/App.jsx
+++ uncontrolled-component/src/App.jsx
@@ -2,14 +2,22 @@ import { useRef } from "react";
 
 export default function App() {
   const nameRef = useRef();
+  const emailRef = useRef();
 
   function handleSubmit() {
     console.log(nameRef.current.value);
+    console.log(emailRef.current.value);
   }
 
   return (
     <div>
+      <label> Name: 
       <input type="text" ref={nameRef} />
+      </label> <br />
+
+      <label>  Email: 
+      <input type="text" ref={emailRef} />
+      </label> <br />
 
       <button onClick={handleSubmit}>
         Submit
